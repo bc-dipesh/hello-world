db51a5efa892aab2766afb4c3d7884375acd26f5
bc-dipesh <bcdipeshwork@gmail.com>
2021-02-10 08:53:39 +0545

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# developing-mobile-apps
+
+## App screenshot
+
+<img src="./screen-shot.png" width=300>
+
+## App running gif
+
+![](./screen-gif.gif)
---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    // activity log tag
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(LOG_TAG, "onCreate(Bundle savedInstanceState) invoked");
+        Log.e(LOG_TAG, "Testing error log");
+    }
+}
